d9e0d64912a7f543052b940094c599952ab6c6a5
sunbo <wjdtnsqh@naver.com>
2022-07-01 12:20:05 +0900

    데이터베이스연결

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 3000;
+const models = require("./models");
 
 // json형식의 데이터를 처리할 수 있게 설정
 app.use(express.json());
@@ -74,6 +75,19 @@ app.get('/products',async (req,res)=>{
     } 
     res.send(result);
 })
+// method get요청이 오고 url은 /product/2로 요청이 온 경우
+app.get('/product/:id', async (req, res)=> {
+    const params = req.params;
+    const { id } = params;
+    const product = {
+            id:id,
+            name:"서버에서 보내는 이름",
+            price:`서버80000`,
+            imgsrc:"images/products/product4.jpg",
+            seller:"서버seller",
+    }
+    res.send(product);
+});
 app.post('/green',async (req,res)=>{
     console.log(req);
     res.send('그린 게시판에 게시글이 입력되었습니다.');
@@ -81,4 +95,17 @@ app.post('/green',async (req,res)=>{
 // 실행
 app.listen(port, ()=>{
     console.log('쇼핑몰 서버가 동작중입니다.');
+    // sequelize와 데이터베이스 연결작업
+    // 데이터베이스 동기화
+    models.sequelize
+    .sync()
+    .then(()=>{
+        console.log("db연결 성공");
+    })
+    .catch((e)=>{
+        console.error(e);
+        console.log("db연결 에러");
+        // 서버실행이 안되면 프로세서 종료
+        process.exit();
+    })
 })
